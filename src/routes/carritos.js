fd43a09e812f30befd8d28b384551d3d51836b0d
AnaPaulaRamon <apramong@uwaterloo.ca>
2021-12-09 21:54:34 -0500

    changes

--- src/routes/carritos.js
+++ src/routes/carritos.js
@@ -13,7 +13,6 @@ router.use((req,res,next)=>{
 //GETS
 router.get('/:id/productos',(req,res)=>{
     let id= parseInt(req.params.id);
-    console.log('esto es req.params.uid', req.params.id)
     contenedor.getCartById(id).then(result=>{
         res.send(result.payload);
     })
@@ -22,7 +21,6 @@ router.get('/:id/productos',(req,res)=>{
 // Crea un carrito y devuelve su id.
 router.post('/',(req,res)=>{
     let carrito = req.body;
-    console.log('este es el carrito que se quiere postear',carrito);
     contenedor.saveCart(carrito).then(result=>{
         res.send(result);
     })
@@ -31,9 +29,7 @@ router.post('/',(req,res)=>{
 // Para incorporar productos al carrito por su id de producto
 router.post('/:id/productos',(req,res)=>{
     let id = req.params.id;
-    console.log('este es la id del carrito al que se quiere añadir al carrito', id);
     let productos = req.body;
-    console.log('este es el producto que se quiere postear al carrito',productos);
     let pid = productos.id;
     contenedor.addProductToCart(id,pid).then(result=>{
         res.send(result);
@@ -53,9 +49,7 @@ router.delete('/:id',(req,res)=>{
 //Elimina un producto del carrito dada la id del carrito y del producto
 router.delete('/:id/productos/:id_prod',(req,res)=>{
     let id= parseInt(req.params.id);
-    console.log('esto el la id del carrito',id);
     let id_prod= parseInt(req.params.id_prod);
-    console.log('esto el la id del producto',id_prod);
     contenedor.deleteProductFromCart(id,id_prod).then(result=>{
         res.send(result);
     })
